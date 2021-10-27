51dc50d257f7b5dd593c57d21b3bb90c6f29ca8d
LordAntonio99 <tonicintora@gmail.com>
2021-10-27 09:49:39 -0400

    ya funciona

--- 001_iniciales/005.sh
+++ 001_iniciales/005.sh
@@ -1,11 +1,43 @@
-#!/bin/bash
-
-# Comprobar solo 2 param
-# Si ?# != 2, $1 = CANTIDAD, $2 = CUELGAN
-# Si $1 = Cantidad:
-## ls /dev/tt* | wc -l > numero.txt
-## ls /dev/tt* > nombre.txt
 # Si $2 = Cuelgan
 ## Bucle
 ## ls /bin/*[t,e] >> nombre.txt
 ## bucle cat nombre.txt
+
+#!/bin/bash
+if [ $# -ne 1 -a $# -ne 2 ]
+then
+    set "CANTIDAD" "CUELGAN"
+fi
+if [ -e "nombre.txt" ]
+then
+    rm "nombre.txt"
+elif [ -e "numero.txt" ]
+then
+    rm "numero.txt"
+fi
+if [ "$1" = "CANTIDAD" ]
+then
+    TOTAL=$(ls /dev/tt* | wc -l)
+    echo "Hay un total de $TOTAL dispositivos con los parametros definidos"
+    echo "$TOTAL" > numero.txt
+    echo "Introduciendo los siguientes nombres en el archivo:"
+    for POS in $(seq 1 1 $TOTAL)
+    do
+            DISPO=$(ls /dev/tt* | head -n $POS | tail -n 1)
+            echo "$DISPO"
+            echo "$DISPO" >> nombre.txt
+            sleep 1
+    done
+fi
+if [ "$2" = "CUELGAN" ]
+then
+    TOTAL=$(find /dev -maxdepth 1 -name "*[t,e]" | wc -l)
+    echo "Introduciendo los siguientes dispositivos al archivo: "
+    for POS in $(seq 1 1 $TOTAL)
+    do
+        LINEA=$(find /dev -maxdepth 1 -name "*[t,e]" | head -n $POS | tail -n 1)
+        echo "$LINEA"
+        echo "$LINEA" >> nombre.txt
+        sleep 1        
+    done
+fi
