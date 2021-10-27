cc1cdaf901a6c32e9b205294b984e8ebb6224ffc
Antonio Cintora Romero <tonicintora@gmail.com>
2021-10-27 12:30:10 +0200

    mas ejercicios

--- 001_iniciales/004.sh
+++ 001_iniciales/004.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+clear
+NOMBRE=""
+while [ "$NOMBRE" = "" ]
+do
+    read -p "Introduce un nombre de usuario:"$'\n>> ' NOMBRE
+    clear
+done
+PASSWD=$(grep "$NOMBRE" /etc/passwd)
+if [ $? -ne 0 ]
+then
+    echo "El usuario introducido no existe"
+else
+    echo "Buenos dias $NOMBRE, tu usuario aparece en el sistema."
+    LONG=$(env | wc -l)
+    echo "El entorno tiene $LONG variables, que son:"
+    for POS in $(seq 1 1 $LONG)
+    do
+        env | head -n $POS | tail -n 1
+        sleep 1
+    done
+fi
---
+++ 001_iniciales/005.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Comprobar solo 2 param
+# Si ?# != 2, $1 = CANTIDAD, $2 = CUELGAN
+# Si $1 = Cantidad:
+## ls /dev/tt* | wc -l > numero.txt
+## ls /dev/tt* > nombre.txt
+# Si $2 = Cuelgan
+## Bucle
+## ls /bin/*[t,e] >> nombre.txt
+## bucle cat nombre.txt
