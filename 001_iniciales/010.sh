8d9f66f70dad4d6d36daed1bd2c7652a552b2de7
ProgramadorNovato <tonicintora@gmail.com>
2021-11-02 12:21:46 +0100

    añadidos nuevos ejercicios

--- 001_iniciales/010.sh
+++ 001_iniciales/010.sh
@@ -3,5 +3,16 @@ clear
 INPUT=""
 while [ "$INPUT" = "" ]
 do
-    read -p "Introduce las iniciales de usuario: " INPUT
+    read -p "Introduce las iniciales de usuario: "$'\n>> ' INPUT
 done
+
+USUARIOS=$(who | cut -d" " -f1 | grep "^$INPUT")
+NUMUSUARIOS=$(echo "$USUARIOS" | wc -l)
+
+if [ "$USUARIOS" = "" ]
+then
+    echo "No hay ningun usuario que empiece por dichas credenciales y/o este conectado."
+else
+    echo "Los usuarios conectados con dichas iniciales son: "
+    echo $USUARIOS 
+fi
