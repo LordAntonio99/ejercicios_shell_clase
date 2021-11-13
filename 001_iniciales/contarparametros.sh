9b94f97ca3c9a97b3f5616c7c24f0e830a41824d
Programador Novato <tonicintora@gmail.com>
2021-11-13 13:45:04 -0500

    Delete 001_iniciales directory

--- 001_iniciales/contarparametros.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-
-NPARAMS=0
-for PARA in $*
-do
-    NPARAMS=$[$NPARAMS + 1]
-done
-echo "Has introducido un total de $NPARAMS parametros"
--- 001_iniciales/pruebas.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true
-do
-    echo "Hola"
-done
